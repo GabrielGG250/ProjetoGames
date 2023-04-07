6dbafa09138e4f071e858fb4ba152714c514969e
GabrielGG250 <gabe.academic@gmail.com>
2023-04-07 13:05:29 -0300

    Mudando o Tema

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const botoesCarrossel = document.querySelectorAll('.botao');
-const imagens = document.querySelectorAll('.imagem')
-const logo = document.querySelectorAll('.logo')
+const imagens = document.querySelectorAll('.imagem');
+const logo = document.querySelectorAll('.logo');
+const descricao = document.querySelectorAll('.descricao');
 
 botoesCarrossel.forEach((botao, indice) => {
     botao.addEventListener('click', () => {
@@ -11,6 +12,7 @@ botoesCarrossel.forEach((botao, indice) => {
         botao.classList.add('selecionado');
 
         const imagemAtiva = document.querySelector('.ativa');
+
         imagemAtiva.classList.remove('ativa');
 
         imagens[indice].classList.add('ativa');
@@ -20,5 +22,11 @@ botoesCarrossel.forEach((botao, indice) => {
         logoSelecionada.classList.remove('ativa1');
 
         logo[indice].classList.add('ativa1');
+
+        const descricaoAtiva = document.querySelector('.ativa2');
+
+        descricaoAtiva.classList.remove('ativa2');
+
+        descricao[indice].classList.add('ativa2')
     })
 })
